b111f177adf7784a26e19dd0c418567e1049d5b0
brigsul <bri.g.sullivan@gmail.com>
2017-04-02 16:09:31 -0400

    Final Version

---
+++ assets/js/todo.js
@@ -0,0 +1,30 @@
+// Check Off Specific Todos By Clicking
+
+$('ul').on('click', 'li', function() {
+    $(this).toggleClass('done');
+});
+
+// Remove Todos That Have Been Deleted
+
+$ ('ul').on('click', 'span', function(event) {
+    $(this).parent().fadeOut(500,function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// Add New Todos
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + '</li>');
+    }
+});
+
+// Add Button Click Listener
+
+$('.fa-pencil-square-o').click(function() {
+    $("input[type='text']").fadeToggle();
+});
